91147a0f5d180f09e95ae51b6f0de4223be49469
gityangshuai <java_yangshuai@163.com>
2019-03-08 17:34:43 +0800

    新增常量

---
+++ src/main/java/org/yangs/common/constant/Constants.java
@@ -0,0 +1,53 @@
+package org.yangs.common.constant;
+
+/**
+ * 通用常量信息配置
+ * @author YangS
+ * @create 2019-03-08  17:13
+ */
+public class Constants {
+    /**
+     * UTF-8字符集设置
+     */
+    public static final String UTF8 = "URF-8";
+    /**
+     * 成功标识
+     */
+    public static final String SUCCESS = "0";
+    /**
+     * 失败标识
+     */
+    public static final String FAIL = "1";
+    /**
+     * 登录成功
+     */
+    public static final String LOGIN_SUCCESS = "Success";
+    /**
+     * 登出、退出、注销
+     */
+    public static final String LOGOUT = "Logout";
+    /**
+     * 登录失败
+     */
+    public static final String LOGIN_FAIL = "Error";
+    /**
+     * 自动去除表前缀
+     */
+    public static String AUTO_REOMVE_PRE = "true";
+    /**
+     * 第几页
+     */
+    public static String PAGE_NUM = "pageNum";
+    /**
+     * 每页显示行数
+     */
+    public static String PAGE_SIZE = "pageSize";
+    /**
+     * 排序列
+     */
+    public static String ORDER_BY_COLUMN = "orderByColumn";
+    /**
+     * 正序、倒序，asc或desc
+     */
+    public static String IS_ASC = "isAsc";
+}
---
+++ src/main/java/org/yangs/common/constant/PermissionConstants.java
@@ -0,0 +1,33 @@
+package org.yangs.common.constant;
+
+/**
+ * 权限使用的常量信息
+ * @author YangS
+ * @create 2019-03-08  17:24
+ */
+public class PermissionConstants {
+    /**
+     * 新增
+     */
+    public static final String ADD_PERMISSION = "add";
+    /**
+     * 修改编辑
+     */
+    public static final String EDIT_PERMISSION = "edit";
+    /**
+     * 删除
+     */
+    public static final String REMOVE_PERMISSION = "remove";
+    /**
+     * 导出
+     */
+    public static final String EXPORT_PERMISSION = "export";
+    /**
+     * 显示
+     */
+    public static final String VIEW_PERMISSION = "view";
+    /**
+     * 查询
+     */
+    public static final String LIST_PERMISSION = "list";
+}
